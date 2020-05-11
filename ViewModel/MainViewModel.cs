9639170535b04dcd0c939368d0140ef32ffb44c8
holmat98 <62332336+holmat98@users.noreply.github.com>
2020-05-11 12:37:23 +0200

    app view and drives list

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using miniTC.ViewModel.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Input;
+
+namespace miniTC.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string[] _drivesList;
+
+        public string[] DriveList
+        {
+            get => _drivesList;
+            set
+            {
+                _drivesList = value;
+                onPropertyChanged(nameof(DriveList));
+            }
+        }
+
+
+
+        private ICommand _getDrivesList = null;
+        public ICommand GetDrivesList
+        {
+            get
+            {
+                if(_getDrivesList == null)
+                {
+                    _getDrivesList = new RelayCommand(
+                        arg => { DriveList = Environment.GetLogicalDrives(); },
+                        arg => true
+                        );
+                }
+                return _getDrivesList;
+            }
+        }
+
+    }
+}
