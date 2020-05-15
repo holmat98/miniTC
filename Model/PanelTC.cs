a44eda4d1e05311ff531a1635ec8aecad575fd19
holmat98 <62332336+holmat98@users.noreply.github.com>
2020-05-15 18:32:49 +0200

    added directories

---
+++ Model/PanelTC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniTC.Model
+{
+    class PanelTC
+    {
+        private string _currentPath;
+        private string[] _drivesList;
+        private string[] _pathList;
+
+        public string CurrentPath
+        {
+            get => _currentPath;
+            set
+            {
+                _currentPath = value;
+            }
+        }
+
+        public string[] DriveList
+        {
+            get => _drivesList;
+            set
+            {
+                _drivesList = value;
+            }
+        }
+
+        public string[] PathList
+        {
+            get => _pathList;
+            set
+            {
+                _pathList = value;
+            }
+        }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -6,38 +6,125 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using miniTC.Model;
 
 namespace miniTC.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
-        private string[] _drivesList;
+        private string[] _leftdrivesList;
 
-        public string[] DriveList
+        public string[] LeftDrivesList
         {
-            get => _drivesList;
+            get => _leftdrivesList;
             set
             {
-                _drivesList = value;
-                onPropertyChanged(nameof(DriveList));
+                _leftdrivesList = value;
+                onPropertyChanged(nameof(LeftDrivesList));
+            }
+        }
+
+        private string[] _rightdrivesList;
+
+        public string[] RightDrivesList
+        {
+            get => _rightdrivesList;
+            set
+            {
+                _rightdrivesList = value;
+                onPropertyChanged(nameof(RightDrivesList));
+            }
+        }
+
+        private string _pathFromLList;
+        public string PathFromLList
+        {
+            get => _pathFromLList;
+            set
+            {
+                _pathFromLList = value;
+                onPropertyChanged(nameof(PathFromLList));
+            }
+        }
+
+        private string _currentPath;
+        public string CurrentPath
+        {
+            get => _currentPath;
+            set
+            {
+                _currentPath = value;
+                onPropertyChanged(nameof(CurrentPath));
+            }
+        }
+
+        private List<string> _leftPathList;
+        public List<string> LeftPathList
+        {
+            get => _leftPathList;
+            set
+            {
+                _leftPathList = value;
+                onPropertyChanged(nameof(LeftPathList));
             }
         }
 
 
-
-        private ICommand _getDrivesList = null;
-        public ICommand GetDrivesList
+        private ICommand _getRightDrivesList = null;
+        public ICommand GetRightDrivesList
         {
             get
             {
-                if(_getDrivesList == null)
+                if (_getRightDrivesList == null)
                 {
-                    _getDrivesList = new RelayCommand(
-                        arg => { DriveList = Environment.GetLogicalDrives(); },
+                    _getRightDrivesList = new RelayCommand(
+                        arg => {
+                            RightDrivesList = new string[Environment.GetLogicalDrives().Length];
+                            RightDrivesList = Environment.GetLogicalDrives(); },
                         arg => true
                         );
                 }
-                return _getDrivesList;
+                return _getRightDrivesList;
+            }
+        }
+
+        private ICommand _getLeftDrivesList = null;
+        public ICommand GetLeftDrivesList
+        {
+            get
+            {
+                if (_getLeftDrivesList == null)
+                {
+                    _getLeftDrivesList = new RelayCommand(
+                        arg => {
+                            LeftDrivesList = new string[Environment.GetLogicalDrives().Length];
+                            LeftDrivesList = Environment.GetLogicalDrives(); },
+                        arg => true
+                        );
+                }
+                return _getLeftDrivesList;
+            }
+        }
+
+        private ICommand _selectionChanged = null;
+        public ICommand SelectionChanged
+        {
+            get
+            {
+                if(_selectionChanged == null)
+                {
+                    _selectionChanged = new RelayCommand(
+                        arg => { 
+                            CurrentPath = PathFromLList;
+                            LeftPathList = Directory.GetFiles(PathFromLList).ToList<string>();
+                            LeftPathList.AddRange(Directory.GetDirectories(PathFromLList).ToList<string>());
+                        },
+                        arg => PathFromLList != null
+                        );
+                }
+
+                return _selectionChanged;
             }
         }
 
